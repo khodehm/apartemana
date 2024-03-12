eeaab915c5866161f1313cc23b5b9cbd35de638a
aliabbasi__1374 <aliabbasi9674@gmail.com>
2024-03-12 15:05:39 +0330

    style edits 2024/12/3

--- src/app/layouts/landing-page/page-one/landing-page.component.ts
+++ src/app/layouts/landing-page/page-one/landing-page.component.ts
@@ -12,23 +12,7 @@ import {
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent implements AfterViewInit {
-  @ViewChild('arrow', { read: ElementRef }) arrow!: ElementRef;
-  @HostListener('window.scroll', ['$event'])
-  onScroll(event: Event) {
-    console.log(event);
-  }
-  ngAfterViewInit(): void {
-
-  }
+export class LandingPageComponent implements OnInit {
+  ngOnInit() {}
   constructor() {}
-
-  scrollToMiddle() {
-    const ScrollMiddle: number = window.innerHeight / 2;
-    this.arrow.nativeElement.scroll({
-      top: ScrollMiddle,
-      behavior: 'smooth',
-    });
-    console.log(this.scrollToMiddle);
-  }
 }
